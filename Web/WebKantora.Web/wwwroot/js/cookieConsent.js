621d8c441253703910854d54f5d6769e4c43463a
Viliana Zhilkova <vilianazhilkova@abv.bg>
2018-03-25 10:31:13 +0300

    Moved cookie consent script to a separate file.

---
+++ Web/WebKantora.Web/wwwroot/js/cookieConsent.js
@@ -0,0 +1,19 @@
+﻿window.addEventListener("load", function () {
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#d6cfcf"
+            },
+            "button": {
+                "background": "#3d3434"
+            }
+        },
+        "theme": "classic",
+        "content": {
+            "message": "Този сайт използва бисквитки(cookies).",
+            "dismiss": "Съгласен съм",
+            "link": "Научи повече",
+            "href": "www.webkantora.bg/cookiespolicy"
+        }
+    })
+});
