bb0e1f3299c93bb4d2be3265890cd1d3c3d95d07
Viliana Zhilkova <vilianazhilkova@abv.bg>
2018-04-09 11:30:05 +0300

    Refactoring (BlogController).

--- Web/WebKantora.Web/Controllers/BlogController.cs
+++ Web/WebKantora.Web/Controllers/BlogController.cs
@@ -5,8 +5,9 @@ using System;
 using System.Threading.Tasks;
 using PagedList.Core;
 
-using WebKantora.Web.Infrastructure.Mappings;
+using WebKantora.Data.Models;
 using WebKantora.Services.Data.Contracts;
+using WebKantora.Web.Infrastructure.Mappings;
 using WebKantora.Web.Models.ArticleViewModels;
 using System.Linq;
 using System.Collections.Generic;
@@ -20,53 +21,109 @@ namespace WebKantora.Web.Controllers
         private IArticlesService articlesService;
         private IMapper mapper;
         private IMemoryCache cache;
+        private ICustomErrorService customErrors;
 
-        public BlogController(IArticlesService articlesService, IMapper mapper, IMemoryCache cache)
+        public BlogController(IArticlesService articlesService, IMapper mapper, IMemoryCache cache, ICustomErrorService customErrors)
         {
             this.articlesService = articlesService;
             this.mapper = mapper;
             this.cache = cache;
+            this.customErrors = customErrors;
         }
 
         [HttpGet]
-        public ActionResult Index(int page = 1)
+        public async Task<ActionResult> Index(int page = 1)
         {
-            // Keyword[] parameter
-            if (!cache.TryGetValue("ArticlesCache", out ICollection<ArticleViewModel> cacheEntry))
+            try
             {
-                cacheEntry = this.articlesService.GetAll()
-                .To<ArticleViewModel>().ToList();
+                // Keyword[] parameter
+                if (!cache.TryGetValue("ArticlesCache", out ICollection<ArticleViewModel> cacheEntry))
+                {
+                    cacheEntry = this.articlesService.GetAll()
+                    .To<ArticleViewModel>().ToList();
 
-                cache.Set("ArticlesCache", cacheEntry);
+                    cache.Set("ArticlesCache", cacheEntry);
+                }
+                var viewModel = cacheEntry.AsQueryable().ToPagedList(page, 5);
+                return this.View(viewModel);
             }
-            var viewModel = cacheEntry.AsQueryable().ToPagedList(page, 5);
-            return this.View(viewModel);
+            catch(Exception ex)
+            {
+                var error = new CustomError()
+                {
+                    InnerException = "",
+                    Message = ex.Message,
+                    Source = ex.Source,
+                    StackTrace = ex.StackTrace,
+                    CustomMessage = ""
+                };
+
+                await this.customErrors.Add(error);
+                return this.View();
+            }
+
         }
 
         [HttpGet]
         public async Task<ActionResult> ById(Guid id)
         {
-            var article = await this.articlesService.GetById(id);
-            var viewModel = this.mapper.Map<FullArticleViewModel>(article);
-            var keywordArticles = article.KeywordArticles;
-
-            foreach (var ka in keywordArticles)
+            try
             {
-                this.GetSeoHelper().AddMetaKeyword(ka.Keyword.Content);
+                var article = await this.articlesService.GetById(id);
+                var viewModel = this.mapper.Map<FullArticleViewModel>(article);
+                var keywordArticles = article.KeywordArticles;
+
+                foreach (var ka in keywordArticles)
+                {
+                    this.GetSeoHelper().AddMetaKeyword(ka.Keyword.Content);
+                }
+
+                return this.View(viewModel);
+            }
+            catch(Exception ex)
+            {
+                var error = new CustomError()
+                {
+                    InnerException = "",
+                    Message = ex.Message,
+                    Source = ex.Source,
+                    StackTrace = ex.StackTrace,
+                    CustomMessage = ""
+                };
+
+                await this.customErrors.Add(error);
+                return this.View();
             }
 
-            return this.View(viewModel);
         }
 
         [HttpGet]
-        public ActionResult ByKeyword(Guid keywordId, int page = 1)
+        public async Task<ActionResult> ByKeyword(Guid keywordId, int page = 1)
         {
-            var viewModel = this.articlesService.GetByKeyword(keywordId)
-                .To<ArticleViewModel>().ToPagedList(page, 2);
+            try
+            {
+                var viewModel = this.articlesService.GetByKeyword(keywordId)
+                    .To<ArticleViewModel>().ToPagedList(page, 2);
 
-            ViewBag.KeywordId = keywordId;
+                ViewBag.KeywordId = keywordId;
 
-            return this.View(viewModel);
+                return this.View(viewModel);
+            }
+            catch(Exception ex)
+            {
+                var error = new CustomError()
+                {
+                    InnerException = "",
+                    Message = ex.Message,
+                    Source = ex.Source,
+                    StackTrace = ex.StackTrace,
+                    CustomMessage = ""
+                };
+
+                await this.customErrors.Add(error);
+
+                return this.View();
+            }
         }
     }
 }
